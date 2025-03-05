0b17eef9211ea10b784b4763d44baecc400dac80
faustojc <faustojcboko@gmail.com>
2025-03-05 17:34:19 +0800

    Fixed tab responsiveness

--- resources/js/Components/NavStatus.tsx
+++ resources/js/Components/NavStatus.tsx
@@ -1,4 +1,7 @@
-import { Link, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { useMemo, useState } from "react";
+import useIsMobile from "@/Hooks/useIsMobile";
+import { CloseToMenuTransition, MenuToCloseTransition } from "@/Components/Icons";
 
 interface NavTab {
     name: string;
@@ -13,15 +16,36 @@ interface NavStatusProps {
 }
 
 const NavStatus = ({currTab, setCurrTab, tabs}: NavStatusProps) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [otherTabs, setOtherTabs] = useState<NavTab[]>([]);
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const isMobile = useIsMobile();
+
+    const displayTabs = useMemo(() => {
+        if (!isMobile) return tabs;
+
+        if (tabs.length > 3) {
+            setOtherTabs(tabs.slice(2));
+            return tabs.slice(0, 2);
+        }
+
+        return tabs;
+    }, [tabs, isMobile]);
+
     return (
         <Navbar
             isBordered
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
             classNames={{
                 item: [
                     "flex",
                     "relative",
                     "h-full",
                     "items-center",
+                    "sm:justify-start",
+                    "justify-between",
                     "data-[active=true]:after:content-['']",
                     "data-[active=true]:after:absolute",
                     "data-[active=true]:after:bottom-0",
@@ -34,7 +58,7 @@ const NavStatus = ({currTab, setCurrTab, tabs}: NavStatusProps) => {
             }}
         >
             <NavbarContent className="flex gap-4" justify="start">
-                {tabs.map((tab) => {
+                {displayTabs.map((tab) => {
                     if (tab.notFor != null && tab.notFor()) {
                         return null;
                     }
@@ -51,6 +75,26 @@ const NavStatus = ({currTab, setCurrTab, tabs}: NavStatusProps) => {
                     )
                 })}
             </NavbarContent>
+            {isMobile && otherTabs.length > 0 && (
+                <Dropdown onOpenChange={setIsOpen}>
+                    <DropdownTrigger>
+                        <Button isIconOnly variant="light">
+                            {isOpen ? <MenuToCloseTransition /> : <CloseToMenuTransition />}
+                        </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu aria-label="Other tabs" items={otherTabs}>
+                        {(tab) => (
+                            <DropdownItem
+                                key={tab.label}
+                                className={currTab === tab.name ? "text-primary" : ""}
+                                onPress={() => setCurrTab(tab.name)}
+                            >
+                                {tab.label}
+                            </DropdownItem>
+                        )}
+                    </DropdownMenu>
+                </Dropdown>
+            )}
         </Navbar>
     )
 }
---
+++ resources/js/Hooks/useIsMobile.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+
+const useIsMobile = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth <= 768);
+
+        window.addEventListener('resize', handleResize);
+
+        // Cleanup the event listener on unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return isMobile;
+};
+
+export default useIsMobile;
