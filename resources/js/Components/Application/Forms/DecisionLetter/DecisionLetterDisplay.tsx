90107af38fec75d3c75c35fc9c33bc75cbf736fd
faustojc <faustojcboko@gmail.com>
2025-03-05 17:35:10 +0800

    Adjust placement of button

--- resources/js/Components/Application/Forms/DecisionLetter/DecisionLetterDisplay.tsx
+++ resources/js/Components/Application/Forms/DecisionLetter/DecisionLetterDisplay.tsx
@@ -17,24 +17,22 @@ export const DecisionLetterDisplay: React.FC<{decisionLetter: DecisionLetter}> =
     return (
         <div className="bg-default-100 p-4 rounded-lg">
             <div className="flex flex-col sm:gap-0 gap-5">
-                <div className="flex flex-col gap-0 sm:flex-row sm:justify-between items-center sm:gap-3">
+                <div className="flex flex-col gap-2 sm:flex-row sm:justify-between items-start sm:gap-3">
                     <div className="flex items-center gap-2">
                         <h4 className="font-medium">{decisionLetter.file_name}</h4>
                         {decisionLetter.is_signed && (
                             <Chip color="success" variant="flat" size="sm">Signed</Chip>
                         )}
                     </div>
-                    <div className="flex gap-2">
-                        <Button
-                            as={Link}
-                            href={route('decision-letter.download', {decision_letter: decisionLetter})}
-                            color="primary"
-                            variant="flat"
-                            startContent={<CloudArrowDown className="w-4 h-4" />}
-                        >
-                            Download
-                        </Button>
-                    </div>
+                    <Button
+                        as={Link}
+                        href={route('decision-letter.download', {decision_letter: decisionLetter})}
+                        color="primary"
+                        variant="flat"
+                        startContent={<CloudArrowDown className="w-4 h-4" />}
+                    >
+                        Download
+                    </Button>
                 </div>
                 <p className="text-sm text-default-600">
                     Uploaded on {formatDate(decisionLetter.date_uploaded)}
