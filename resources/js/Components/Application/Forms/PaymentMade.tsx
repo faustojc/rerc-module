cf087ffb7b20b33acc8075fffe29278eaf52e5dd
faustojc <faustojcboko@gmail.com>
2025-03-05 17:34:50 +0800

    Fixed link overflowing

--- resources/js/Components/Application/Forms/PaymentMade.tsx
+++ resources/js/Components/Application/Forms/PaymentMade.tsx
@@ -77,6 +77,7 @@ const PaymentMade = ({user, application, status, handleUpdateApplication}: Appli
                                     color="primary"
                                     href={route('applications.payment-download', {application: application})}
                                     className="px-0 py-2"
+                                    style={{ overflowWrap: 'anywhere' }}
                                 >
                                     {application.proof_of_payment_url.split('\\').pop()?.split('/').pop()}
                                 </Link>
