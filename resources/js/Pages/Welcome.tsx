1953293919bd9dbb9e65ab0cbcbb5894eefc75a5
faustojc <faustojcboko@gmail.com>
2025-03-25 12:38:04 +0800

    Removed unused function

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -19,16 +19,6 @@ import { ProcessStep } from "@/Components/ProcessStep";
 export default function Welcome({
     auth,
 }: PageProps<{ laravelVersion: string; phpVersion: string }>) {
-    const handleImageError = () => {
-        document
-            .getElementById('screenshot-container')
-            ?.classList.add('!hidden');
-        document.getElementById('docs-card')?.classList.add('!row-span-1');
-        document
-            .getElementById('docs-card-content')
-            ?.classList.add('!flex-row');
-        document.getElementById('background')?.classList.add('!hidden');
-    };
 
     return (
         <>
